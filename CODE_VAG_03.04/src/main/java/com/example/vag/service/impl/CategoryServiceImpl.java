76068b8ebb8dff2693eba24e4f759446f9ce833a
MaryZhminkovskaya <maryad0102@mail.ru>
2025-04-03 14:10:51 +0300

    есть авторизация, можно криво создать публикации, нет кнопок перехода между страницами, у пользователя в профиле нет публикаций

---
+++ CODE_VAG_03.04/src/main/java/com/example/vag/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.vag.service.impl;
+
+import com.example.vag.model.Category;
+import com.example.vag.repository.CategoryRepository;
+import com.example.vag.service.CategoryService;
+import com.example.vag.controller.dto.CategoryDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Category category) {
+        categoryRepository.delete(category);
+    }
+
+    @Override
+    public Category update(CategoryDTO categoryDTO) {
+        Category category = categoryRepository.findById(categoryDTO.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+        category.setName(categoryDTO.getName());
+        category.setDescription(categoryDTO.getDescription());
+        return categoryRepository.save(category);
+    }
+}
